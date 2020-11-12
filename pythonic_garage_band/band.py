267cc869763c4ce8596721982d7ca64d6eb818d8
Hexx King <hexxking13@gmail.com>
2020-11-12 13:58:01 -0800

    test_drummer_repr is passing

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -26,3 +26,6 @@ class Bassist:
 class Drummer(Musician):
     def __str__(self):
       return f"My name is {self.name} and I play drums"
+
+    def __repr__(self):
+      return f"Drummer instance. Name = {self.name}"
