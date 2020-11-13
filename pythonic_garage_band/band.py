d7c45ef5062ee4cfba49eac9bf0202da0df31ba4
Hexx King <hexxking13@gmail.com>
2020-11-13 10:42:18 -0800

    test_to_list is passing. Assignment complete

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -1,9 +1,10 @@
 class Band:
-    list = []
+    instances = []
 
-    def __init__(self, name, members=0):
+    def __init__(self, name, members=None):
         self.name = name
         self.members = members
+        Band.instances.append(self.name)
 
     def __str__(self):
         return f"The band {self.name}"
@@ -14,20 +15,28 @@ class Band:
     def play_solos(self, members=None):
       solos = []
       for member in self.members:
-          solos.append(member.play_solos)
-      return solos 
+          solos.append(member.play_solo())
+      return [memeber.play_solo() for memeber in self.members]
+      #alternatively
+      # return solos 
 
     @classmethod
     def to_list(cls):
-      return cls.list
+      return cls.instances
 
 
 class Musician():
-    def __init__(self, name, instrument, solo):
+    def __init__(self, name, instrument=None, solo=None):
         self.name = name
         self.instrument = instrument
         self.solo = solo
 
+    def __repr__(self):
+        return f"{self.__class__.__name__} instance. Name = {self.name}"
+
+    def __str__(self):
+      return f"My name is {self.name} and I play {self.get_instrument()}"
+    
     def get_instrument(self):
         return self.instrument
 
@@ -39,30 +48,12 @@ class Guitarist(Musician):
     def __init__(self, name):
         super().__init__(name, "guitar", "face melting guitar solo") 
 
-    def __str__(self):
-      return f"My name is {self.name} and I play guitar"
-
-    def __repr__(self):
-      return f"Guitarist instance. Name = {self.name}"
-
-
 class Bassist(Musician):
     def __init__(self, name):
         super().__init__(name, "bass", "bom bom buh bom") 
-
-    def __str__(self):
-      return f"My name is {self.name} and I play bass"
-    
-    def __repr__(self):
-      return f"Bassist instance. Name = {self.name}"
 
 
 class Drummer(Musician):
     def __init__(self, name):
         super().__init__(name, "drums", "rattle boom crash") 
 
-    def __str__(self):
-      return f"My name is {self.name} and I play drums"
-
-    def __repr__(self):
-      return f"Drummer instance. Name = {self.name}"
